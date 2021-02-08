83a6514b642e312318f5c21c67eac7cbe913d67c
kryom-arc <67024644+kryom-arc@users.noreply.github.com>
2021-02-08 18:00:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let theme = 'Bathroom Divination';
+console.log(`Theme: ${theme}.`);
+
+let line = 'You like to pee with the door open';
+
+let message = {
+    _person: ['Someone', 'You'],
+    _firstVerb: ['like', 'tend', 'hate'],
+    _connection: ['to'],
+    _secondVerb: ['pee', 'shit', 'shower'],
+    _blabla: ['with the door open', 'with people watching', 'in the dark', 'naked']
+}
