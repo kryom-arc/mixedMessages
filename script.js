092cbe294b6144671ab048cd05448fa7d1f94a98
kryom-arc <67024644+kryom-arc@users.noreply.github.com>
2021-02-08 18:08:09 -0300

    added getters to object

--- script.js
+++ script.js
@@ -5,8 +5,23 @@ let line = 'You like to pee with the door open';
 
 let message = {
     _person: ['Someone', 'You'],
+    get person(){
+        return this._person;
+    },
     _firstVerb: ['like', 'tend', 'hate'],
+    get firstVerb(){
+        return this._firstVerb;
+    },
     _connection: ['to'],
+    get connection(){
+        return this._connection;
+    },
     _secondVerb: ['pee', 'shit', 'shower'],
-    _blabla: ['with the door open', 'with people watching', 'in the dark', 'naked']
+    get secondVerb(){
+        return this._secondVerb;
+    },
+    _peculiarity: ['with the door open', 'with people watching', 'in the dark', 'naked'],
+    get peculiarity(){
+        return this._peculiarity;
+    },
 }
